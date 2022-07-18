f1ba39e6ecf2f48ef93207b94242503a21327069
Max Holman <max@holmn.com>
2022-07-18 14:42:05 +0800

    fix: export AlsStore to assist with avoiding TS problems in consumers

--- lib/index.ts
+++ lib/index.ts
@@ -1,2 +1,6 @@
 export { ServerTiming } from './server-timing.js';
-export { createTimingStore } from './create-store.js';
+
+// type AlsStore be exported because of
+// https://github.com/microsoft/TypeScript/issues/42873
+// consumers will need to import it to avoid that error
+export { type AlsStore, createTimingStore } from './create-store.js';
