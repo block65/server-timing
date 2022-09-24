fcc940e94e34ed5cd81660e47ef5604421d8c560
Max Holman <max@holmn.com>
2022-09-24 15:23:38 +0800

    chore: renamed for IDE compat

--- prettier.config.mjs
+++
@@ -1,8 +0,0 @@
-/**
- * WARN: this file will blow up jest if it's not named .mjs
- */
-
-// eslint-disable-next-line import/no-extraneous-dependencies, import/extensions
-import config from '@block65/eslint-config/prettier.config.js';
-
-export default config;
